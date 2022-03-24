5ae4562f4ffd124cbd022f91e800a5bc3d6699b3
Gioele Maruccia <gioelemaruccia8@gmail.com>
2022-03-24 10:57:36 +0100

    Add final section: Drawing Fractal Images

---
+++ Project - Drawing Fractal Images/BitmapFileHeader.h
@@ -0,0 +1,23 @@
+//
+// Created by Gioele Maruccia on 24/03/22.
+//
+
+#ifndef PROJECT___DRAWING_FRACTAL_IMAGES_BITMAPFILEHEADER_H
+#define PROJECT___DRAWING_FRACTAL_IMAGES_BITMAPFILEHEADER_H
+
+#include <cstdint> /* Standard variables dimensions are provided so to avoid that the arbitrary used Compiler could change variable characteristics */
+
+using namespace std;
+
+#pragma pack(2)
+
+struct BitmapFileHeader {
+    char header[2]{'B', 'M'};
+    int32_t fileSize;
+    int32_t reserved{0};
+    int32_t dataOffset;
+};
+
+
+
+#endif //PROJECT___DRAWING_FRACTAL_IMAGES_BITMAPFILEHEADER_H
---
+++ Project - Drawing Fractal Images/Fractal Creator - Hello World.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Gioele Maruccia on 24/03/22.
+//
+#include <iostream>
+#include "BitmapFileHeader.h"
+
+using namespace std;
+
+int main() {
+    cout << "!!!Hello World" << endl;
+    return 0;
+}
